33a3f70fc502d75ef96ff88e5256d31ef52e0e04
Vikram Pawar <vikram.pawar@resonate.tech>
2023-08-10 20:23:36 +0100

    01 ch using inline style sheet

--- pawarv/code/DemoApp/src/app/app.component.ts
+++ pawarv/code/DemoApp/src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // Using Inline Template
   // templateUrl: './app.component.html',
   template: `<h1> The title is {{title}} </h1>`,
-  styleUrls: ['./app.component.css']
+
+  // styleUrls: ['./app.component.css']
+  // Using Inline Style
+  styles: [`h1 { color: red; }`]
 })
 export class AppComponent {
   title = 'DemoApp by Vikram';
