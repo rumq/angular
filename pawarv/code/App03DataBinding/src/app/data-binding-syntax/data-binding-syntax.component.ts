ac2aa13d203fa2cf137f4891959e6a0f94e44514
Vikram Pawar <vikram.pawar@resonate.tech>
2023-08-12 11:42:23 +0100

    Add basic databinding, verbose flag, label

--- pawarv/code/App03DataBinding/src/app/data-binding-syntax/data-binding-syntax.component.ts
+++ pawarv/code/App03DataBinding/src/app/data-binding-syntax/data-binding-syntax.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+class Car {
+  constructor(public make: string, public model: string, public year: number) {}
+}
+
 @Component({
   selector: 'app-data-binding-syntax',
   templateUrl: './data-binding-syntax.component.html',
   styleUrls: ['./data-binding-syntax.component.css'],
 })
 export class DataBindingSyntaxComponent {
-  firstName = 'Vikram';
+  firstName: string;
+  lastName: string;
+  nationality: string;
+  emailAddress: string;
+  companyCar?: Car;
+  salary: number;
+  verbose: boolean;
+
+  constructor() {
+    this.firstName = 'Amitabh';
+    this.lastName = 'Bachchan';
+    this.nationality = 'Indian';
+    this.emailAddress = 'amitabh.bachchan@gmail.com';
+    this.companyCar = new Car('BMW', 'X5', 2019);
+    this.salary = 1000000;
+    this.verbose = true;
+  }
+  label() {
+    return this.verbose ? 'Show Brief' : 'Show Verbose';
+  }
 }
