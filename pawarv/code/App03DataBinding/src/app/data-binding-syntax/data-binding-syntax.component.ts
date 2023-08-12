a672ba921a93753295f7718e439d41ad97ffe7af
Vikram Pawar <vikram.pawar@resonate.tech>
2023-08-12 10:18:53 +0100

    Data binding - simple just first name

--- pawarv/code/App03DataBinding/src/app/data-binding-syntax/data-binding-syntax.component.ts
+++ pawarv/code/App03DataBinding/src/app/data-binding-syntax/data-binding-syntax.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-data-binding-syntax',
   templateUrl: './data-binding-syntax.component.html',
-  styleUrls: ['./data-binding-syntax.component.css']
+  styleUrls: ['./data-binding-syntax.component.css'],
 })
 export class DataBindingSyntaxComponent {
-
+  firstName = 'Vikram';
 }
