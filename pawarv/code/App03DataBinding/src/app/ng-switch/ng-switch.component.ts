db8f80f34047f4bccc63f326f781ddef60d7cb06
Vikram Pawar <vikram.pawar@resonate.tech>
2023-08-14 19:52:12 +0100

    Add ngSwitch final

--- pawarv/code/App03DataBinding/src/app/ng-switch/ng-switch.component.ts
+++ pawarv/code/App03DataBinding/src/app/ng-switch/ng-switch.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./ng-switch.component.css']
 })
 export class NgSwitchComponent {
+  firstName: string;
+  lastName: string;
+  nationality: string;
+  emailAddress: string;
+  salary: number;
+  skills: Array<string>;
 
+  constructor() {
+    this.firstName = 'Amitabh';
+    this.lastName = 'Bachchan';
+    // this.nationality = 'Indian';
+    // this.nationality = 'UK';
+    this.nationality = 'UNKNOWN';
+    this.emailAddress = 'amitabh.bachan@gmail.com';
+    this.salary = 30000;
+    this.skills = ['Java', 'MQ', 'AWS'];
+  }
+
+  payRise() {
+    this.salary = this.salary + 4000;
+  }
 }
