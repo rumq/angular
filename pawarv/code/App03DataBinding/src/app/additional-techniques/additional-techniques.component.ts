90072b34d5fc9c35526f66a61d5777186b605ac7
Vikram Pawar <vikram.pawar@resonate.tech>
2023-08-14 20:15:23 +0100

    Additional techniques final

--- pawarv/code/App03DataBinding/src/app/additional-techniques/additional-techniques.component.ts
+++ pawarv/code/App03DataBinding/src/app/additional-techniques/additional-techniques.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-additional-techniques',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./additional-techniques.component.css']
 })
 export class AdditionalTechniquesComponent {
+  name = "Amitabh Bacchan";
+  skills = "Skills :";
+
+  addSkill(skill: HTMLInputElement) {
+    // get the value from the html input and set the data component
+    this.skills += skill.value + " ";
+    // reset the html input field
+    skill.value = "";
+    skill.focus();
+  }
 
 }
+
