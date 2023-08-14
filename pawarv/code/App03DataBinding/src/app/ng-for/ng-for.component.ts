d080d313afad26e3a31f73c121ea6897c396d423
Vikram Pawar <vikram.pawar@resonate.tech>
2023-08-14 19:36:02 +0100

    Add *ngFor code

--- pawarv/code/App03DataBinding/src/app/ng-for/ng-for.component.ts
+++ pawarv/code/App03DataBinding/src/app/ng-for/ng-for.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./ng-for.component.css']
 })
 export class NgForComponent {
+  firstName: string;
+  lastName: string;
+  nationality: string;
+  emailAddress: string;
+  salary: number;
+  skills: Array<string>;
+
+  constructor() {
+    this.firstName = 'Amitabh';
+    this.lastName = 'Bachchan';
+    this.nationality = 'Indian';
+    this.emailAddress = 'amitabh.bachan@gmail.com';
+    this.salary = 30000;
+    this.skills = ['Java', 'MQ', 'AWS'];
+  }
+
+  payRise() {
+    this.salary = this.salary + 4000;
+  }
 
 }
